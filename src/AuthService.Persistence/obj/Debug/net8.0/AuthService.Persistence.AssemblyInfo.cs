5addaea640bf6466feada7c40983b51f6120b135
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2026-03-16 10:15:13 -0600

    feat(application): Crear logica de autenticacion y cuentas de usuario

--- src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
+++ src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea4b8c8d3171d6c508d986a5ad946fb1cea5ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbefdf810c5bd9f9b2c324b393727fa60cf205c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
